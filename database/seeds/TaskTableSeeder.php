4e256ec955f32d7b7ddf9bd22437a889d5a5cb51
linhphp <thuclinh997@gmail.com>
2020-09-29 13:25:32 +0700

    create model migrate seed

---
+++ database/seeds/TaskTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Task;
+class TaskTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     *@return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create(); 
+        for($i=0; $i<10; $i++){
+            Task::create(
+                ['name' => $faker->name()]
+            ); 
+        }
+    }
+}
